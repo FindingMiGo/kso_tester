f6c8e0a8b171021ab71509f855d2e2e48f9020c9
tisoya <tisoya@student.42tokyo.jp>
2021-10-22 17:17:12 +0900

    fix bug

--- ft_printf_tester.c
+++ ft_printf_tester.c
@@ -3,6 +3,9 @@
 #include<string.h>
 #include"../ft_printf.h"
 
+#define FT_OFF 0
+#define FT_ON 1
+
 static char	g_num[10];
 char		*flag = "#0+ -";
 char		acc[6][4] = { "" , "." , ".0" , ".1", ".10", ".15"};
@@ -56,7 +59,7 @@ void	ft_printf_tester_int(char *fmt, int d, int flg)
 			free(tmp2);
 			printf("%s[d = %d],\t", fmt_d, d);
 			fflush(stdout);
-			if (flg == 1)
+			if (flg == FT_ON)
 				r = ft_printf(fmt_d, d);
 			else
 				r = printf(fmt_d, d);
@@ -91,9 +94,9 @@ void	ft_printf_tester_char(char *fmt, char c, int flg)
 			free(tmp2);
 			printf("%s[d = %d],\t", fmt_c, c);
 			fflush(stdout);
-			if (flg == 1)
+			if (flg == FT_OFF)
 				r = printf(fmt_c, c);
-			else
+			else if (flg == FT_ON)
 				r = ft_printf(fmt_c, c);
 			fflush(stdout);
 			printf(",[%d]\n", r);
@@ -126,7 +129,7 @@ void	ft_printf_tester_po(char *fmt, unsigned long p, int flg)
 			free(tmp2);
 			printf("%s[d = %ld],\t", fmt_c, p);
 			fflush(stdout);
-			if (flg == 1)
+			if (flg == FT_OFF)
 				r = printf(fmt_c, p);
 			else
 				r = ft_printf(fmt_c, p);
@@ -161,7 +164,7 @@ void	ft_printf_tester_hex(char *fmt, long long p, int flg)
 			free(tmp2);
 			printf("%s[x = %lld],\t", fmt_c, p);
 			fflush(stdout);
-			if (flg == 1)
+			if (flg == FT_OFF)
 				r = printf(fmt_c, p);
 			else
 				r = ft_printf(fmt_c, p);
@@ -196,7 +199,7 @@ void	ft_printf_tester_hex2(char *fmt, long long p, int flg)
 			free(tmp2);
 			printf("%s[x = %lld],\t", fmt_c, p);
 			fflush(stdout);
-			if (flg == 1)
+			if (flg == FT_OFF)
 				r = printf(fmt_c, p);
 			else
 				r = ft_printf(fmt_c, p);
@@ -231,7 +234,7 @@ void	ft_printf_tester_u(char *fmt, unsigned int u, int flg)
 			free(tmp2);
 			printf("%s[u = %d],\t", fmt_u, u);
 			fflush(stdout);
-			if (flg == 1)
+			if (flg == FT_OFF)
 				r = printf(fmt_u, u);
 			else
 				r = ft_printf(fmt_u, u);
@@ -266,7 +269,7 @@ void	ft_printf_tester_str(char *fmt, char *str, int flg)
 			free(tmp2);
 			printf("%s[s = %s],\t", fmt_s, str);
 			fflush(stdout);
-			if (flg == 1)
+			if (flg == FT_OFF)
 				r = printf(fmt_s, str);
 			else
 				r = ft_printf(fmt_s, str);
@@ -288,7 +291,7 @@ void	ft_printf_tester(char *fmt, int flg)
 	int testc[] = {'0', '0' - 256, '0' + 256, ' ', '1', '2', '3', 'a'};
 	unsigned long testp[] = {-1, 1, 15, 16, 17, LONG_MIN, LONG_MAX, INT_MIN, INT_MAX, ULONG_MAX, -ULONG_MAX, 0};
 	long long	testx[] = {-100, -1, 1, 15, 16, 17, 99, 100, LONG_MIN, LONG_MAX, INT_MIN, INT_MAX, ULONG_MAX, -ULONG_MAX, 0, 9223372036854775807LL};
-	char		tests[5][3] = { "" , "0" , "1", "10", "15"};
+	char		tests[6][3] = {"" , "0" , "1", "10", "15"};
 	long long	testu[] = {-1, 1, 15, 16, 17, LONG_MIN, LONG_MAX, INT_MIN, INT_MAX, ULONG_MAX, -ULONG_MAX, 0};
 	size_t	i = 0;
 
@@ -297,6 +300,7 @@ void	ft_printf_tester(char *fmt, int flg)
 	i = 0;
 	while (i < (sizeof(testc)/sizeof(int)))
 		ft_printf_tester_char(fmt, testc[i++], flg);
+	ft_printf_tester_char(fmt, 0, flg);
 	i = 0;
 	while (i < (sizeof(testp)/sizeof(unsigned long)))
 		ft_printf_tester_po(fmt, testp[i++], flg);
@@ -309,6 +313,7 @@ void	ft_printf_tester(char *fmt, int flg)
 	i = 0;
 	while (i < (sizeof(tests)/sizeof(char*)))
 		ft_printf_tester_str(fmt, tests[i++], flg);
+	ft_printf_tester_str(fmt, NULL, flg);
 	i = 0;
 	while (i < (sizeof(testu)/sizeof(long long)))
 		ft_printf_tester_u(fmt, testu[i++], flg);
@@ -346,8 +351,8 @@ int main(int argc, char const *argv[])
 {
 	(void)argv;
 	if (argc > 1)
-		ft_printf_combn(1);
+		ft_printf_combn(FT_ON);
 	else
-		ft_printf_combn(0);
+		ft_printf_combn(FT_OFF);
 	return 0;
 }
